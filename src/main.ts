364a89e7ca6d2c1855fee62af42196ce64cf4d12
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-21 18:10:31 +0700

    feat: add global prefix, validation pipe, and listeing port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { Env } from 'src/config/env-loader';
+
+const { PORT } = Env();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
+  app.enableCors();
+
+  await app.listen(PORT || 3009, () => {
+    Logger.debug(`server runnning at port ${PORT || 3009}`);
+  });
 }
 bootstrap();
