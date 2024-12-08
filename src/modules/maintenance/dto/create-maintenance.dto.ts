32ee92244fba00f4f65c6ea78ef2803fdc1c63d9
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2024-12-08 16:33:45 +0700

    feat: change transform isMaintenance

--- src/modules/maintenance/dto/create-maintenance.dto.ts
+++ src/modules/maintenance/dto/create-maintenance.dto.ts
@@ -18,11 +18,10 @@ export enum BogieType {
   Bogie_2_1 = '2.1',
   Bogie_2_2 = '2.2',
   Bogie_2_3 = '2.3',
-  Bogie_2_4 = '2.4'
+  Bogie_2_4 = '2.4',
 }
 
 export class CreateUpdateMaintenanceDto {
-
   @ApiProperty({
     example: 'b9c63c51-20ae-4fe6-9a39-9641e7d4ecc5',
     description: 'The id of the Asset',
@@ -40,6 +39,15 @@ export class CreateUpdateMaintenanceDto {
   flow?: string;
 
   @ApiProperty({
+    example: 'W37780',
+    description: 'Work Order Number',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  wo_number?: string;
+
+  @ApiProperty({
     example: 'Program Maintenance',
     description: 'The Program of maintenance',
     required: false,
@@ -52,9 +60,12 @@ export class CreateUpdateMaintenanceDto {
     description: 'Status Maintenance',
     required: false,
   })
-  @Transform(({ value }) => (value === undefined || value === null ? undefined : value === 'true' || value === true))
+  @Transform(({ value }) =>
+    value === undefined || value === null
+      ? undefined
+      : value === 'true' || value === true,
+  )
   @IsBoolean()
   @IsOptional()
   is_maintenance?: boolean;
-
 }
