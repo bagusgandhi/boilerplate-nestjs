32ff68355e819a6f487899e1304723284d3a4c78
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-01-15 16:51:17 +0700

    feat: update maintenance log by id

--- src/modules/maintenance-log/maintenance-log.controller.ts
+++ src/modules/maintenance-log/maintenance-log.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { MaintenanceLogService } from './maintenance-log.service';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PaginationDto } from 'src/global/dto/pagination.dto';
 import { UuidParamDto } from 'src/global/dto/params-id.dto';
+import { GetUser, IUserRequest } from 'src/decorators/get-user.decorator';
+import { CreateMaintenanceLogDto } from './dto/create-maintenance-log.dto';
 
 @ApiTags('Maintenance Log')
 @Controller('maintenance-log')
@@ -25,5 +27,15 @@ export class MaintenanceLogController {
   @Get(':id')
   async get(@Param() params: UuidParamDto) {
     return this.maintenanceLogService.get(params.id as string);
+  }
+
+  @ApiOperation({
+    summary: 'Update maintenance log by id.',
+  })
+  @ApiBearerAuth()
+  // @Permissions('assetManagement.updateAsset')
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() body: CreateMaintenanceLogDto, @GetUser() user: IUserRequest) {
+    return this.maintenanceLogService.update(id, body, user);
   }
 }
