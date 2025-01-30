0dc8115669846b7d70b55e7d6d18d22b8325a34c
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-01-30 18:19:12 +0700

    feat: create api key strategy

---
+++ src/modules/auth/strategy/apikey.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class ApiKeyStrategy extends PassportStrategy(
+  HeaderAPIKeyStrategy,
+  'api-key',
+) {
+  constructor(private authService: AuthService) {
+    super(
+      { header: 'api-key', prefix: '' },
+      true,
+      async (apiKey: string, done: any) => {
+        if (this.authService.validateApiKey(apiKey)) {
+          done(null, true);
+        } else {
+          done(null, false);
+        }
+      },
+    );
+  }
+}
