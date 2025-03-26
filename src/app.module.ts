626bca6d4a9f70761045f8316208d9f8de64648e
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-27 06:44:49 +0700

    feat: update app module and env-loader to include new modules and Cloudflare/Registrar environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,17 @@ import { JwtAuthGuard } from './modules/auth/guard/jwt.guard';
 import { UploadsModule } from './modules/uploads/uploads.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Env } from './config/env-loader';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { InvoiceModule } from './modules/invoice/invoice.module';
+import { DomainModule } from './modules/domain/domain.module';
+import { PromoModule } from './modules/promo/promo.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { TemplateModule } from './modules/templates/template.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { CloudflareModule } from './modules/cloudflare/cloudflare.module';
+import { SitesModule } from './modules/sites/sites.module';
+import { RegistrarModule } from './modules/registrar/registrar.module';
 
 const { EMAIL_HOST, EMAIL_USERNAME, EMAIL_PASSWORD } = Env();
 
@@ -31,21 +42,32 @@ const { EMAIL_HOST, EMAIL_USERNAME, EMAIL_PASSWORD } = Env();
         },
       },
     }),
+    ProductsModule,
+    OrdersModule,
+    InvoiceModule,
+    DomainModule,
+    PromoModule,
+    CategoriesModule,
+    TemplateModule,
+    PaymentModule,
+    CloudflareModule,
+    SitesModule,
+    RegistrarModule,
   ],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard
+      useClass: JwtAuthGuard,
     },
     {
       provide: APP_GUARD,
-      useClass: RolesGuard
+      useClass: RolesGuard,
     },
     {
       provide: APP_GUARD,
-      useClass: PermissionsGuard
-    }
+      useClass: PermissionsGuard,
+    },
   ],
 })
 export class AppModule {}
