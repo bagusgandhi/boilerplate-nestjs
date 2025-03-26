2d0bf87778aacd688d8e07508db7382dbd2d4228
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2025-03-27 06:38:45 +0700

    feat: add TemplateCategory entity to manage relationships between templates and categories

---
+++ src/modules/templates/entities/template-category.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, BaseEntity } from 'typeorm';
+import { Templates } from './templates.entity';
+import { Categories } from 'src/modules/categories/entities/categories.entity';
+
+@Entity('template_category')
+export class TemplateCategory extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Templates, (template) => template.templateCategories)
+  template: Templates;
+
+  @ManyToOne(() => Categories, (category) => category.templateCategories)
+  category: Categories;
+}
