84ce72361778c934fbdaed14ab186807d721f34c
Bagus Gandhi <bagusgandhi4@gmail.com>
2025-05-30 13:25:27 +0700

    feat: add corporate_documents relationship to Uploads entity for enhanced document management

--- src/modules/uploads/entities/uploads.entity.ts
+++ src/modules/uploads/entities/uploads.entity.ts
@@ -1,5 +1,6 @@
-import { BusinessPermits } from 'src/modules/business_permits/entities/business-permits.entity';
+import { BusinessPermits } from 'src/modules/business-permits/entities/business-permits.entity';
 import { Contract } from 'src/modules/contract/entities/contract.entity';
+import { CorporateDocuments } from 'src/modules/corporate-documents/entities/corporate-documents.entity';
 import { User } from 'src/modules/user/entities/user.entity';
 import {
   BaseEntity,
@@ -31,6 +32,9 @@ export class Uploads extends BaseEntity {
 
   @ManyToOne(() => BusinessPermits, (businessPermits) => businessPermits.uploads, { onDelete: 'SET NULL' })
   business_permits: BusinessPermits;
+
+  @ManyToOne(() => CorporateDocuments, (corporateDocuments) => corporateDocuments.uploads, { onDelete: 'SET NULL' })
+  corporate_documents: CorporateDocuments;
 
   @ManyToOne(() => User, (user) => user.uploads, { onDelete: 'SET NULL' })
   user: User;
