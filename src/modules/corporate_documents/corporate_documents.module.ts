0c2f4eb1c3e093107b75cc800828546ae7b40edb
Bagus Gandhi <bagusgandhi4@gmail.com>
2025-05-30 13:22:35 +0700

    refactor: remove BusinessPermits and CorporateDocuments modules, including controllers, services, and related DTOs for a cleaner codebase

--- src/modules/corporate_documents/corporate_documents.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CorporateDocumentsController } from './corporate_documents.controller';
-import { CorporateDocumentsService } from './corporate_documents.service';
-
-@Module({
-  controllers: [CorporateDocumentsController],
-  providers: [CorporateDocumentsService]
-})
-export class CorporateDocumentsModule {}
