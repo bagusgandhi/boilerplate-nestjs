6305ba9fddf49853fea318d527d92ebf36095a4e
Bagus Gandhi <bagusgandhi4@gmail.com>
2025-05-30 09:30:42 +0700

    feat: integrate TypeORM and additional modules into business permits module for enhanced functionality

--- src/modules/business_permits/business_permits.module.ts
+++ src/modules/business_permits/business_permits.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BusinessPermitsController } from './business_permits.controller';
 import { BusinessPermitsService } from './business_permits.service';
-
+import { StepProgressModule } from '../step-progress/step-progress.module';
+import { UserModule } from '../user/user.module';
+import { UploadsModule } from '../uploads/uploads.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BusinessPermits } from './entities/business-permits.entity';
+import { BusinessPermitsHistory } from './entities/business-permits-history.entity';
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      BusinessPermits,
+      BusinessPermitsHistory,
+    ]),
+    StepProgressModule,
+    UserModule,
+    UploadsModule,
+  ],
   controllers: [BusinessPermitsController],
   providers: [BusinessPermitsService]
 })
