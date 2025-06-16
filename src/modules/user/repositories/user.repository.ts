57d0ff19bba64334f2520b61232dd7e0a92a3609
Bagus Gandhi <bagusgandhi4@gmail.com>
2025-06-16 10:34:29 +0700

    feat: enhance queryPaginate with viewAll flag and add findAllByRoleName and findAllNameAndEmail methods

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -66,6 +66,7 @@ export class UserRepository extends Repository<User> {
     limit: number,
     search: string,
     roleId: string[],
+    viewAll: boolean = false, // Add the viewAll flag
   ) {
     try {
       const query = this.createQueryBuilder('user')
@@ -78,34 +79,71 @@ export class UserRepository extends Repository<User> {
           'user.updated_at',
           'role.id',
           'role.name'
-        ])
-
+        ]);
+  
+      // Apply role filter if provided
       if (roleId && roleId.length > 0) {
         query.andWhere('role.id IN (:...roleId)', { roleId });
       }
-
+  
+      // Apply search filter if provided
       if (search) {
         query.andWhere(
           'user.email LIKE :search OR user.name LIKE :search OR role.name LIKE :search',
           { search: `%${search}%` },
         );
       }
-
-      query
-      .orderBy('user.created_at', 'DESC')
-      .skip((page - 1) * limit)
-      .take(limit);
-
+  
+      // Conditionally apply pagination based on the viewAll flag
+      if (!viewAll) {
+        query
+          .orderBy('user.created_at', 'DESC')
+          .skip((page - 1) * limit)
+          .take(limit);
+      }
+  
+      // Get the results and total count
       const [results, total] = await query.getManyAndCount();
-
+  
+      // If viewAll is true, set total to the number of results returned
+      const totalPages = viewAll ? 1 : Math.ceil(total / limit);
+  
       return {
         page,
         total,
         results,
+        totalPages,
       };
     } catch (error) {
-      console.log(error)
+      console.log(error);
       throw new InternalServerErrorException('Error fetching user data');
     }
   }
+  
+  async findAllByRoleName(roleName: string) {
+    try {
+      const users = await this.find({ where: { roles: { name: roleName } } });
+      return users;
+    } catch (error) {
+      throw new InternalServerErrorException('Error fetching user data');
+    }
+  }
+
+  async findAllNameAndEmail(){
+    try {
+      const query = this.createQueryBuilder('user')
+        .select([
+          'user.id',
+          'user.name',
+          'user.email',
+        ]);
+
+      const users = await query.getMany();
+
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
 }
